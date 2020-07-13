9b1c3eee8aff90f8fe6b1ace936e2de6dd61a832
Piotr Sołtysek <you@example.com>
2020-07-13 21:30:33 +0200

    Faker dependency

--- src/main/java/pl/camp/it/services/FakerDataService.java
+++ src/main/java/pl/camp/it/services/FakerDataService.java
@@ -1,45 +1,32 @@
 package pl.camp.it.services;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Random;
+import com.github.javafaker.Faker;
 
 public class FakerDataService implements IRandomDataService {
+    Faker faker = new Faker();
 
     @Override
     public String generateCity() {
-        List<String> cities = Arrays.asList("Kraków", "Warszawa", "Poznań",
-                "Katowice", "Gliwice", "New York", "Paris", "Berlin", "London", "Tokyo");
-        return cities.get(new Random().nextInt(cities.size()));
+        return faker.address().city();
     }
 
     @Override
     public String generateStreet() {
-        List<String> streets = Arrays.asList("Polna", "Ogrodowa", "Malinowa",
-                "Szewska", "Topolowa", "Floriańska", "Wall Street", "Krzywa", "Prosta");
-        return streets.get(new Random().nextInt(streets.size()));
+        return faker.address().streetName();
     }
 
     @Override
     public String generateInvoiceSingature() {
-        Random random = new Random();
-        int no = random.nextInt(10) + 1;
-        int month = random.nextInt(12) + 1;
-        int year = random.nextInt(6) + 2015;
-
-        return new StringBuilder().append("FV/").append(no).append("/")
-                .append(month).append("/").append(year).toString();
+        return faker.number().digit();
     }
 
     @Override
     public String generateProductName() {
-        List<String> names = Arrays.asList("Woda", "Wino", "Kiełbasa", "Piwo", "Salceson", "Bułka", "Czipsy", "Cola");
-        return names.get(new Random().nextInt(names.size()));
+        return faker.beer().name();
     }
 
     @Override
     public double generateProductPrice() {
-        int generatedNumber = new Random().nextInt(1901) + 100;
-        return ((double) generatedNumber) / 100.00;
+        return faker.number().randomDouble(2, 1, 20);
     }
 }
